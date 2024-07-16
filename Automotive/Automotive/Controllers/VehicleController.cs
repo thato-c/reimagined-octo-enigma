3a08cc381a146c995edeeab5ff0e96f24095b86f
Thato <t.chitja369@gmail.com>
2024-07-16 22:26:25 +0200

    Migrate Database

---
+++ Automotive/Automotive/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Automotive.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
