7bf44d27b9112bd9a3ec74aad92fd424e083d8b2
Thato <t.chitja369@gmail.com>
2024-07-16 21:17:00 +0200

    Setup application

---
+++ Automotive/Automotive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Automotive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
