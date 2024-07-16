63c6c649eab33c35b7fa9f95d262adfa5c0d7a00
Thato <t.chitja369@gmail.com>
2024-07-16 21:48:18 +0200

    Add Models

---
+++ Automotive/Automotive/Models/Service.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Automotive.Models
+{
+    public class Service
+    {
+        public Guid ServiceId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public int Price { get; set; } = 0;
+
+        public int LaborHours { get; set; } = 0;
+
+        public int WarrantyInMonths { get; set; } = 0;
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        public ICollection<VehicleService> VehicleServices { get; set; } = new List<VehicleService>();
+    }
+}
