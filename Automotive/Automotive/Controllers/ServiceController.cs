495cc867f80ed4c1cc027a728a0935ee9260db5e
Thato <t.chitja369@gmail.com>
2024-07-17 14:15:22 +0200

    Add Edit actions

--- Automotive/Automotive/Controllers/ServiceController.cs
+++ Automotive/Automotive/Controllers/ServiceController.cs
@@ -121,5 +121,173 @@ namespace Automotive.Controllers
                 return View();
             }
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid serviceId)
+        {
+            try
+            {
+                var service = await serviceRepository.GetServiceByIdAsync(serviceId);
+
+                if (service == null)
+                {
+                    ViewBag.Message = "The service has not been found";
+                    return View();
+                }
+
+                var viewModel = new ServiceDetailViewModel
+                {
+                    ServiceId = service.ServiceId,
+                    Name = service.Name,
+                    Description = service.Description,
+                    Price = service.Price,
+                    LaborHours = service.LaborHours,
+                    WarrantyInMonths = service.WarrantyInMonths,
+                    RowVersion = service.RowVersion,
+                };
+
+                return View(viewModel);
+            }
+            catch (DbUpdateException ex)
+            {
+                // Log the exception details
+                _logger.LogError(ex, "An error occurred while retrieving data from the database.");
+
+                // Optionally, log additional details
+                if (ex.InnerException != null) 
+                {
+                    _logger.LogError("Inner Exception: {Message}", ex.InnerException.Message);
+                }
+                if (ex.InnerException?.InnerException != null)
+                {
+                    _logger.LogError("SQL: {Message}", ex.InnerException.InnerException.Message);
+                }
+
+                ModelState.AddModelError("", "An error occurred while retrieving data from the database.");
+                ViewBag.Message = "An error occurred while retrieving data from the database.";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ServiceDetailViewModel viewModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var serviceToEdit = await serviceRepository.GetServiceByIdAsync(viewModel.ServiceId);
+
+                    if (serviceToEdit != null)
+                    {
+                        if (serviceToEdit.Name == viewModel.Name &&
+                            serviceToEdit.Description == viewModel.Description &&
+                            serviceToEdit.Price == viewModel.Price &&
+                            serviceToEdit.LaborHours == viewModel.LaborHours &&
+                            serviceToEdit.WarrantyInMonths == viewModel.WarrantyInMonths)
+                        {
+                            ModelState.AddModelError(string.Empty, "Data has not been modified.");
+                            return View(viewModel);
+                        }
+
+                        else
+                        {
+                            serviceRepository.SetOriginalRowVersion(serviceToEdit, viewModel.RowVersion);
+
+                            if (await TryUpdateModelAsync<Service>(
+                                serviceToEdit,
+                                "",
+                                s => s.Name, s => s.Description, s => s.Price, s => s.LaborHours, s => s.WarrantyInMonths))
+                            {
+                                try
+                                {
+                                    serviceRepository.UpdateService(serviceToEdit);
+                                    serviceRepository.Save();
+                                }
+                                catch (DbUpdateConcurrencyException ex)
+                                {
+                                    var exceptionEntry = ex.Entries.Single();
+                                    var databaseEntry = exceptionEntry.GetDatabaseValues();
+                                    if (databaseEntry == null)
+                                    {
+                                        ModelState.AddModelError(string.Empty, "Unable to save changes. The service was deleted by another user.");
+                                        return View(viewModel);
+                                    }
+                                    else
+                                    {
+                                        var databaseValues = (Service)databaseEntry.ToObject();
+                                        var updatedService = new ServiceDetailViewModel
+                                        {
+                                            ServiceId = databaseValues.ServiceId,
+                                            Name = databaseValues.Name,
+                                            Description = databaseValues.Description,
+                                            Price = databaseValues.Price,
+                                            LaborHours = databaseValues.LaborHours,
+                                            WarrantyInMonths = databaseValues.WarrantyInMonths,
+                                            RowVersion = viewModel.RowVersion
+                                        };
+
+                                        if (databaseValues.Name != serviceToEdit.Name)
+                                        {
+                                            ModelState.AddModelError("Name", $"Current Value: {databaseValues.Name}");
+                                        }
+                                        if (databaseValues.Price != serviceToEdit.Price)
+                                        {
+                                            ModelState.AddModelError("Price", $"Current Value: {databaseValues.Price}");
+                                        }
+                                        if (databaseValues.Description != serviceToEdit.Description)
+                                        {
+                                            ModelState.AddModelError("Description", $"Current Value: {databaseValues.Description}");
+                                        }
+                                        if (databaseValues.LaborHours != serviceToEdit.LaborHours)
+                                        {
+                                            ModelState.AddModelError("LaborHours", $"Current Value: {databaseValues.LaborHours}");
+                                        }
+                                        if (databaseValues.WarrantyInMonths != serviceToEdit.WarrantyInMonths)
+                                        {
+                                            ModelState.AddModelError("WarrantyInMonths", $"Current Value: {databaseValues.WarrantyInMonths}");
+                                        }
+
+                                        ModelState.AddModelError(string.Empty, "The record you attempted to edit " +
+                                            "was modofied by another user after you got the original value." +
+                                            "The edit operation was canceled and the current values in the database" +
+                                            "have been displayed. If you still want to edit this record, click " +
+                                            "the Save button again. Otherwise, click the Back to Lisst hyperlink.");
+
+                                        return View(updatedService);
+                                    }
+                                }
+                            }
+                            return RedirectToAction("Index");
+                        }
+                    }
+
+                    ViewBag.Message = "Service was not found";
+                    return View(viewModel);
+                }
+                return View(viewModel);
+            }
+            catch (DbUpdateException ex)
+            {
+                // Log the exception details
+                _logger.LogError(ex, "An error occurred while editing data in the database.");
+
+                // Optionally, log additional details
+                if (ex.InnerException != null)
+                {
+                    _logger.LogError("Inner Exception: {Message}", ex.InnerException.Message);
+                }
+                if (ex.InnerException?.InnerException != null)
+                {
+                    _logger.LogError("SQL: {Message}", ex.InnerException.InnerException.Message);
+                }
+
+                ModelState.AddModelError("", "An error occurred while editing data in the database.");
+                ViewBag.Message = "An error occurred while editing data in the database.";
+                return View();
+            }
+        }
+
     }
 }
---
+++ Automotive/Automotive/ViewModels/ServiceDetailViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Automotive.ViewModels
+{
+    public class ServiceDetailViewModel
+    {
+        public Guid ServiceId { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Price is required")]
+        public int Price { get; set; } = 0;
+
+        [Required(ErrorMessage = "Labour Hours afre required")]
+        public int LaborHours { get; set; } = 0;
+
+        [Required(ErrorMessage = "Warranty is required.")]
+        public int WarrantyInMonths { get; set; } = 0;
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+    }
+}
