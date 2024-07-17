c6a1a77e499b9802fb007132fda2be5d5e1b9880
Thato <t.chitja369@gmail.com>
2024-07-17 02:50:00 +0200

    Add Service Interface and Repository

---
+++ Automotive/Automotive/Interfaces/IServiceRepository.cs
@@ -0,0 +1,16 @@
+﻿using Automotive.Models;
+
+namespace Automotive.Interfaces
+{
+    public interface IServiceRepository:IDisposable
+    {
+        IQueryable<Service> GetServices();
+        Task<Service> GetServiceByIdAsync(Guid serviceId);
+        void InsertService (Service service);
+        Task<Service> DeleteService(Guid serviceId);
+        void UpdateService(Service service);
+        void SetOriginalRowVersion(Service service, byte[] rowVersion);
+        void Save();
+        Task SaveAsync();
+    }
+}
---
+++ Automotive/Automotive/Repositories/ServiceRepository.cs
@@ -0,0 +1,81 @@
+﻿using Automotive.Data;
+using Automotive.Interfaces;
+using Automotive.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Automotive.Repositories
+{
+    public class ServiceRepository : IServiceRepository, IDisposable
+    {
+        private AutomotiveDBContext context;
+
+        public async Task<Service> DeleteService(Guid serviceId)
+        {
+            var service = await context.Services.FindAsync(serviceId);
+
+            if (service != null)
+            {
+                context.Services.Remove(service);
+            }
+
+            return null;
+        }
+
+        public async Task<Service> GetServiceByIdAsync(Guid serviceId)
+        {
+            return await context.Services
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(s => s.ServiceId == serviceId);
+        }
+
+        public IQueryable<Service> GetServices()
+        {
+            return context.Services.AsQueryable();
+        }
+
+        public void InsertService(Service service)
+        {
+            context.Services.Add(service);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public void SetOriginalRowVersion(Service service, byte[] rowVersion)
+        {
+            context.Entry(service).Property("RowVersion").OriginalValue = rowVersion;
+        }
+
+        public void UpdateService(Service service)
+        {
+            context.Entry(service).State = EntityState.Modified;
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
